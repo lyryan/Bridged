5d366bc9a0fe80700f9ecae3919bfa865d588fc0
Ahmed Darkazanli <adarkazanli2@gmail.com>
2020-01-20 21:19:28 -0800

    Completed app routing for home, about, and campaigns pages

--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,49 @@
 /* eslint-disable */
 
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import About from './pages/About.js';
+import Campaigns from './pages/Campaigns'
+import Home from './pages/Home'
 
 export default class App extends React.Component {
   render() {
-    return <h1>Bridged</h1>;
+    return (
+      <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/campaigns">Campaigns</Link>
+            </li>
+          </ul>
+        </nav>
+        {/* A <Switch> looks through its children <Route>s and
+        renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/campaigns">
+            <Campaigns />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        </div>
+        </Router>
+    );
   }
 }
