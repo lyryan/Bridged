07e6400bb4a1f26b776ac877f228d4872d1e1eda
Ryan Ly <ly.ryan@yahoo.com>
2020-01-20 19:27:41 -0800

    Adds initial project setup

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  resolve: {
+    modules: ['src', 'node_modules']
+  },
+  devtool: 'source-map',
+  entry: {
+    vendor: ['react', 'react-dom'],
+    client: './src/index.js'
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: '[name].chunkhash.bundle.js',
+    chunkFilename: '[name].chunkhash.bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.(css)$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    port: 8080
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Bridged',
+      template: './src/index.html',
+      filename: './index.html',
+      inject: true,
+      minify: {
+        collapseWhitespace: true,
+        collapseInlineTagWhitespace: true,
+        minifyCSS: true,
+        minifyURLs: true,
+        minifyJS: true,
+        removeComments: true,
+        removeRedundantAttributes: true
+      }
+    })
+  ]
+};
