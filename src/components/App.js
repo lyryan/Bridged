167e9c00767522d91b30c008dc09192214b1b1cd
Ahmed Darkazanli <adarkazanli2@gmail.com>
2020-01-23 17:07:47 -0800

    Set up app to work with local blockchain and added smart contract instances

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
+/* eslint-disable */
+
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
 import Fortmatic from 'fortmatic';
 import Web3 from 'web3';
+
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Header from './header';
 
@@ -9,20 +12,27 @@ import About from '../pages/About';
 import Campaigns from '../pages/Campaigns';
 import Home from '../pages/Home';
 
-const fm = new Fortmatic(process.env.FORTMATIC_KEY);
+// const fm = new Fortmatic(process.env.FORTMATIC_KEY)
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       isLoggedIn: false,
-      account: '',
+      accounts: [],
       email: '',
     };
   }
 
   componentDidMount = async () => {
+    
+    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545")) 
+    this.getUserData()
+
+    // replace the following lines to use fortmatic
+    /*
     window.web3 = new Web3(fm.getProvider());
+
     const isUserLoggedIn = await fm.user.isLoggedIn();
     if (isUserLoggedIn) {
       this.getUserData();
@@ -30,9 +40,81 @@ class App extends React.Component {
     this.setState({
       isLoggedIn: isUserLoggedIn,
     });
+    */
+
   };
 
-  login = async () => {
+  executeSmartContract = async () => {
+    const ABI = [
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "internalType": "int256",
+            "name": "x",
+            "type": "int256"
+          },
+          {
+            "internalType": "int256",
+            "name": "y",
+            "type": "int256"
+          }
+        ],
+        "name": "add",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "getSum",
+        "outputs": [
+          {
+            "internalType": "int256",
+            "name": "",
+            "type": "int256"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "sum",
+        "outputs": [
+          {
+            "internalType": "int256",
+            "name": "",
+            "type": "int256"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      }
+    ]
+    const address = '0x65Ed94f41C3368c4Cf6a3F690c123d50bEeDA908'
+
+    // Create contract object
+    const tokenContract = await web3.eth.contract(ABI);
+    // Instantiate contract
+    const tokenContractInstance = await tokenContract.at(address);
+    
+    console.log('this is the account', this.state.account)
+    await tokenContractInstance.add(1, 2, { from: this.state.accounts[0] }) // use first account by default
+    
+        // Call contract function (non-state altering) to get total token supply
+    tokenContractInstance.getSum.call(function(error, result) {
+      if (error) throw error;
+      console.log(result);
+    });
+  }
+
+  /*login = async () => {
     await fm.user
       .login()
       .then(account => {
@@ -48,10 +130,15 @@ class App extends React.Component {
     await fm.user.logout().then(() => {
       this.setState({ isLoggedIn: false, account: '', email: '' });
     });
-  };
+  };*/
 
   getUserData = async () => {
-    const userData = await fm.user.getUser();
+    web3.eth.getAccounts((err, accounts) => {
+      if (err != null) console.error("An error occurred: "+err);
+      else this.setState({isLoggedIn: true, accounts}, () => console.log(this.state))
+    });
+
+    /*const userData = await fm.user.getUser();
 
     window.web3.eth.getAccounts((err, accounts) => {
       this.setState({
@@ -60,12 +147,12 @@ class App extends React.Component {
       }).catch(() => {
         console.log('Error retrieving user data', err);
       });
-    });
+    });*/
+
   };
 
   render() {
     const { isLoggedIn, account, email } = this.state;
-
     return (
       <>
         <Header
@@ -80,7 +167,7 @@ class App extends React.Component {
         <Router>
           <div>
             <div>
-              <button
+              {/*<button
                 type="submit"
                 onClick={() => {
                   this.login();
@@ -97,7 +184,7 @@ class App extends React.Component {
                 >
                   Log Out
                 </button>
-              </Link>
+                </Link>*/}
 
               <div
                 style={{
@@ -106,8 +193,9 @@ class App extends React.Component {
                 }}
               >
                 {`isLoggedIn: ${isLoggedIn}`}
-                <div>Public Address: {account}</div>
+              <div>{this.state.accounts.map((val, index) => <p key = {index}>Public address: {val} </p>)}</div>
                 <div>Email: {email}</div>
+                <button onClick ={() => this.executeSmartContract()}>Do Math</button>
               </div>
             </div>
             <Switch>
---
+++ truffle/migrations/1_initial_migration.js
@@ -0,0 +1,7 @@
+const Migrations = artifacts.require("Migrations");
+const Addition = artifacts.require("Addition");
+
+module.exports = function(deployer) {
+  deployer.deploy(Migrations);
+  deployer.deploy(Addition)
+};
