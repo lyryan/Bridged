bb1bba51e1786465ff3970341c8829eaa95cc8e7
VinceVT <vincevutruong@gmail.com>
2020-04-25 13:59:37 -0700

    spinner and fixed up search partially

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import CircularProgress from '@material-ui/core/CircularProgress';
 import SearchIcon from '@material-ui/icons/Search';
 import { crowdfunding, campaign } from '../config';
 import Card from '../components/card';
@@ -12,8 +11,9 @@ const getFilteredList = (arr, input) => {
   // filter by address, campaign title or name of campaign creator
   return arr.filter(element => {
     if (
-      element.address.substring(0, len) === input ||
-      element.campaignTitle.substring(0, len) === input
+      element.address.substring(0, len).toLowerCase() === input.toLowerCase() ||
+      element.campaignTitle.substring(0, len).toLowerCase() ===
+        input.toLowerCase()
     )
       return element;
     return null;
@@ -111,13 +111,23 @@ class Explore extends React.Component {
     return searchResults.map(element => {
       return (
         <div id={styles.searchResults} key={element.address}>
-          <div id={styles.center}>
-            <div style={{ width: '15%', border: '1px solid grey' }}>
+          <div className={styles.center}>
+            <div
+              style={{
+                width: '200px',
+                border: '1px solid grey',
+              }}
+            >
               <Link to={`/campaigns/${element.address}`}>
                 <h3>Title: {element.campaignTitle}</h3>
                 <br />
                 <img
-                  style={{ height: '15%', width: '100%' }}
+                  style={{
+                    height: '15%',
+                    width: '100%',
+                    minWidth: '100px',
+                    minHeight: '50px',
+                  }}
                   alt="campaign"
                   src={`https://ipfs.io/ipfs/${element._photoHash}`}
                 />
@@ -132,9 +142,37 @@ class Explore extends React.Component {
   render() {
     const { searchInput, searchResults, loading } = this.state;
     return (
-      <div>
+      <div className={styles.center}>
         {loading ? (
-          <CircularProgress />
+          <div
+            style={{
+              width: '100%',
+              height: '100%',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              padding: '20%',
+            }}
+          >
+            <div className={styles.spin}>
+              <svg
+                height="50px"
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fas"
+                data-icon="spinner"
+                className="svg-inline--fa fa-spinner fa-w-16"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+              >
+                <path
+                  fill="#adabab"
+                  d="M304 48c0 26.51-21.49 48-48 48s-48-21.49-48-48 21.49-48 48-48 48 21.49 48 48zm-48 368c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zm208-208c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zM96 256c0-26.51-21.49-48-48-48S0 229.49 0 256s21.49 48 48 48 48-21.49 48-48zm12.922 99.078c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48c0-26.509-21.491-48-48-48zm294.156 0c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48c0-26.509-21.49-48-48-48zM108.922 60.922c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.491-48-48-48z"
+                />
+              </svg>
+            </div>
+          </div>
         ) : (
           <div>
             <div id={styles.search}>
